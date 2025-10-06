4900f110cad35ff8961f0e417ee3fee9eb89f2c7
base65-cmd <j.amazige@yahoo.com>
2025-10-06 23:43:13 +0100

    Changed from pgadmin to neon database

--- server/lib/db/db.js
+++ server/lib/db/db.js
@@ -1,14 +1,20 @@
 import pkg from "pg";
+import { Pool } from "@neondatabase/serverless";
 import dotenv from "dotenv";
 dotenv.config();
 
-const { Pool } = pkg;
+// const { Pool } = pkg;
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&channel_binding=require`,
 });
 
 export default pool;
